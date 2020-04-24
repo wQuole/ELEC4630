9fe34920b0ab918c045578ce5ffe5443200f308e
William Kvaale <wgkvaale@gmail.com>
2020-04-24 14:20:42 +0200

    Update utils

--- Hough Transforms and Medial Image Segmentation/utils.py
+++ Hough Transforms and Medial Image Segmentation/utils.py
@@ -1,9 +1,10 @@
+# -*- coding: UTF-8 -*-
 import glob
 import cv2 as cv
+import numpy as np
 from datetime import datetime
 from matplotlib import pyplot as plt
 import matplotlib.pyplot as mpl
-import matplotlib.gridspec as gridspec
 mpl.rcParams['savefig.pad_inches'] = 0
 
 def save_image(destination, images, label=""):
@@ -45,7 +46,7 @@ def save_figures(images, titles=[], rows=1):
     print(f"Saved a figure \U0001F4BE")
 
 
-def show_figures(images, titles=[], rows=1, save=False):
+def show_figures(images, titles=[], rows=1, folder="MRI_Convex", filename="figure", save=False):
     columns = len(images)
     fig = plt.figure(figsize=(32, 32/columns))
     for i in range(1, columns*rows+1):
@@ -57,7 +58,7 @@ def show_figures(images, titles=[], rows=1, save=False):
         plt.imshow(images[i-1], cmap='gray')
     if save:
         now = datetime.now()
-        plt.savefig(f"output/MRI_Convex/convex_{now}.png")
+        plt.savefig(f"{folder}/{filename}_{now}.png")
         print(f"Saved a figure \U0001F4BE")
     plt.show()
 
@@ -77,8 +78,6 @@ def show_figure_snakes(orig_img, inner, outer, save=False):
 def show_images_snakes(images, n_row=4, n_col=4):
     fig, axs = plt.subplots(n_row, n_col, figsize=(10, 10))
     axs = axs.flatten()
-    gs1 = gridspec.GridSpec(4, 4)
-    gs1.update(wspace=0.025, hspace=0.05)  # set the spacing between axes.
     for img, ax in zip(images, axs):
         ax.imshow(img)
         ax.get_xaxis().set_visible(False)
@@ -86,7 +85,7 @@ def show_images_snakes(images, n_row=4, n_col=4):
         ax.set_aspect('equal')
     plt.autoscale(tight=True)
     plt.subplots_adjust(wspace=0, hspace=0)
-    #plt.tight_layout()
+    plt.tight_layout()
     plt.show()
     fig.savefig(f"output/MRI_Snakes/snakes_grid.pdf", bbox_inches='tight', transparent=True, pad_inches=0)
 
@@ -94,3 +93,12 @@ def show_images_snakes(images, n_row=4, n_col=4):
 def get_crop(frame, point1, point2):
     img = frame.copy()
     return img[point1[1]:point1[1]+point2[1], point1[0]:point2[0]+point1[0]]
+
+
+def adjust_gamma(image, gamma=1.0):
+    # source: https://www.pyimagesearch.com/2015/10/05/opencv-gamma-correction/
+    img = image.copy()
+    inverted_gamma = 1.0 / gamma
+    look_up_table = np.array([((i / 255.0) ** inverted_gamma) * 255
+                      for i in np.arange(0, 256)]).astype("uint8")
+    return cv.LUT(img , look_up_table)
