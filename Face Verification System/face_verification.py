c33085323c13fc56b6a179c88cf244c8bf4bb3f3
William Kvaale <wgkvaale@gmail.com>
2020-05-25 17:45:49 +0200

    Add ROC Curve and Confusion Matrix

--- Face Verification System/face_verification.py
+++ Face Verification System/face_verification.py
@@ -1,8 +1,14 @@
-import cv2 as cv
-import face_recognition
+# Local module
 from utilities import plot_faces
 from utilities import load_images
-
+from utilities import plot_roc_curve
+# External modules
+import cv2 as cv
+import face_recognition
+from sklearn.metrics import roc_curve
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+from seaborn import heatmap
 
 CALTECH = "./dataset/unknown_faces"
 ME = "./dataset/known_faces"
@@ -23,13 +29,13 @@ def predict(known_encodings, test_image):
 
     # Iterate through the unknown faces in the test image, and draw bounding box
     correct = False
-    pred = 0
+    prediction = 0
     for (top, right, bottom, left), face_encoding in zip(test_face_locations, test_face_encodings):
         matches = face_recognition.compare_faces(known_encodings, face_encoding)
 
         if True in matches:
             correct = True
-            classification = 1
+            prediction = 1
             name = "William"
             # Draw box
             cv.rectangle(test_image, (left, top), (right, bottom), (0, 255, 0), 2)
@@ -38,6 +44,7 @@ def predict(known_encodings, test_image):
             cv.putText(test_image, name, (left, y), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         else:
             name = "Unknown"
+            prediction = 0
             # Draw box
             cv.rectangle(test_image, (left, top), (right, bottom), (255, 0, 0), 2)
             # Draw label
@@ -45,7 +52,7 @@ def predict(known_encodings, test_image):
             cv.putText(test_image, name, (left, y), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
     # Return labeled images
-    return correct, test_image, classification
+    return correct, test_image, prediction
 
 
 if __name__ == '__main__':
@@ -61,15 +68,25 @@ if __name__ == '__main__':
 
     y_true = [1]*len(known_faces)+[0]*len(unknown_faces)
     y_pred = []
+
     for img in test_images:
-        check, image, prediction = predict(readid_encodings, img)
-        y_pred.append(prediction)
+        check, image, pred = predict(readid_encodings, img)
+        y_pred.append(pred)
         if check:
             me.append(image)
             print("Added image.")
         else:
             not_me.append(image)
 
+    # Plot ROC Curve
+    #fpr, tpr, thresholds = roc_curve(y_true, y_pred)
+    #plot_roc_curve(fpr, tpr)
+
+    # Plot Confusion Matrix
+    conf_matrix = confusion_matrix(y_true, y_pred)
+    heatmap(conf_matrix, annot=True, fmt='d', cbar=False)
+    plt.show()
+
     # Plot template and test sample, side-by-side
-    #for i, result_image in enumerate(me):
-    #    plot_faces([readid_image, result_image], idx=i, n_row=1, n_col=2, save=True)
+    for i, result_image in enumerate(me):
+       plot_faces([readid_image, result_image], idx=i, n_row=1, n_col=2, save=True)
