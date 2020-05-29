9017a2fd7b548e6065b5a618c1b689887caba4b8
William Kvaale <wgkvaale@gmail.com>
2020-05-29 10:23:51 +0200

    Add utils

---
+++ Face Verification System/utilities.py
@@ -0,0 +1,50 @@
+import os
+import matplotlib.pyplot as plt
+from face_recognition import load_image_file
+
+def plot_roc_curve(false_positive_rate, true_positive_rate):
+    plt.plot(
+        false_positive_rate,
+        true_positive_rate,
+        color='orange',
+        label='ROC'
+    )
+    plt.plot(
+        [0, 1], [0, 1],
+        color='darkblue',
+        linestyle='--',
+        label='Random guess'
+    )
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend()
+    plt.show()
+
+
+def plot_faces(images, idx, n_row, n_col, save=False):
+    plt.figure(figsize=(32, 32))
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.imshow(images[i], cmap='gray')
+        plt.xticks(())
+        plt.yticks(())
+        plt.axis('off')
+    if save:
+        plt.savefig(f"output/face_verification_{idx}.pdf", bbox_inches='tight')
+        plt.clf()
+    elif not save:
+        plt.show()
+
+
+def load_images(path, extension=".jpg", single=False):
+    images = []
+    for root, dirs, files in sorted(os.walk(path)):
+        for file in sorted(files):
+            filepath = root + os.sep + file
+            if file.endswith(f"{extension}"):
+                im = load_image_file(filepath)
+                images.append(im)
+    if single:
+        return load_image_file(path)
+    return images
